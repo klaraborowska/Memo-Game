9319b67de309888317d69423cf8e6f1c391dd7df
Klara <klara.borowska@gmail.com>
2018-03-19 20:53:20 +0100

    Add game round settings in "show card" function

--- js/script.js
+++ js/script.js
@@ -1,7 +1,6 @@
 const cardGame = document.querySelector(".card-list");
 
 // shuffle card when click on refresh button
-
 document.querySelector(".restart-button").addEventListener("click", function() {
     shuffleCards();
 });
@@ -18,17 +17,55 @@ function shuffleCards() {
       }
 }
 
-//show card on click
+
+let gameRound = 2;
+let moves = 0;
+let previousSrc;
+
+//add event listener to card list
 cardGame.addEventListener("click", function(event) {
-    if (event.target.nodeName === "DIV") {
-        showCard(event.target, event.target.nextElementSibling);
-    }
+    const frontCard = event.target;
+    const backCard = event.target.nextElementSibling;
+
+    //check if the clicked element is a div with a class "card-front"
+    if (frontCard.nodeName === "DIV" && frontCard.classList.contains("card-front")) {
+
+        //check if the clickd element doesn't have the class "front-inacitive" (in order not to add the second the same class)
+        if (!frontCard.classList.contains("front-inactive")) {
+            frontCard.classList.add("front-inactive");
+            backCard.classList.add("back-active");
+            gameRound -= 1;
+        }
+
+        //happens, when two cards are open
+        if (gameRound === 0) {
+
+            //reset gameround
+            gameRound = 2;
+
+            //add one move
+            moves +=1;
+            console.log("moves: " + moves);
+            
+            //compares, if cards have the same icons
+            if (previousSrc !== backCard.firstElementChild.src) {
+                hideCard(frontCard, backCard);
+                hideCard(previousFront, previousBack);
+            }
+            
+        }
+    
+       
+        previousSrc = backCard.firstElementChild.src;
+        previousFront = frontCard;
+        previousBack = backCard;
+    } 
 });
 
-//function show card
-function showCard(x, y) {
-    if (!x.classList.contains("front-inactive") && !y.classList.contains("back-active")) {
-        x.classList.add("front-inactive");
-        y.classList.add("back-active");
+
+function hideCard(x, y) {
+    if (x.classList.contains("front-inactive") && y.classList.contains("back-active")) {
+        x.classList.remove("front-inactive");
+        y.classList.remove("back-active");
     }
 }
